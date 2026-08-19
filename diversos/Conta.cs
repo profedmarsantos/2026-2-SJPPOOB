96796f0614320bc015abed9ef12b7e74310e97eb
Professor Edmar Santos <edmar.santos@ifsp.edu.br>
2026-08-19 20:09:33 -0300

    chore(publicacao): publicacao completa

---
+++ diversos/Conta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Conta
+{
+    public string Titular = "";
+    decimal Saldo = 0;
+
+    public void MostrarSaldo()
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Titular: {Titular}");
+        Console.WriteLine($"Conta Saldo: {Saldo}");
+    }
+
+    public void Depositar(decimal valor)
+    {
+        Saldo += valor;
+    }
+    
+    public void Sacar(decimal valor)
+    {
+        if (valor <= 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Valor inválido. O valor deve ser maior que 0 (zero).");
+        }
+        else if (valor > Saldo)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Valor inválido. O valor deve ser menor ou igual ao saldo atual.");
+        }
+        else
+        {
+            Saldo -= valor;
+            Console.WriteLine();
+            Console.WriteLine($"O valor {valor} foi sacado com sucesso!");
+        }
+    }
+
+}
---
+++ diversos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Conta a = new Conta();
+        a.Titular = "Fulano Silva";
+
+
+        a.MostrarSaldo();
+
+        a.Depositar(500);
+
+        a.MostrarSaldo();
+
+        a.Sacar(75);
+
+        a.MostrarSaldo();
+    }
+}
